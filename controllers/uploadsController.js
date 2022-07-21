679d4233b0b36c4d97b0b8652114f9e50ef67c77
Mark Farrier <mark.farrier@gmail.com>
2022-07-22 00:03:08 +1200

    First Commit.

---
+++ controllers/uploadsController.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+const uploadProductImageLocal = async (req, res) => {
+	// check if file exists
+	// check format
+	// check size
+
+	// console.log(req.files);
+
+	if (!req.files) {
+		throw new CustomError.BadRequestError('No File Uploaded');
+	}
+
+	const productImage = req.files.image;
+
+	if (!productImage.mimetype.startsWith('image')) {
+		throw new CustomError.BadRequestError('Please Upload Image');
+	}
+
+	const maxSize = 1000000;
+
+	if (productImage.size > maxSize) {
+		throw new CustomError.BadRequestError(
+			'Please upload image smaller than 1KB'
+		);
+	}
+
+	const imagePath = path.join(
+		__dirname,
+		'../public/uploads/' + `${productImage.name}`
+	);
+
+	await productImage.mv(imagePath);
+
+	return res
+		.status(StatusCodes.OK)
+		.json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+const uploadProductImage = async (req, res) => {
+	const result = await cloudinary.uploader.upload(
+		req.files.image.tempFilePath,
+		{
+			use_filename: true,
+			folder: 'file-upload',
+		}
+	);
+	fs.unlinkSync(req.files.image.tempFilePath);
+	return res
+		.status(StatusCodes.OK)
+		.json({ image: { src: result.secure_url } });
+};
+
+module.exports = {
+	uploadProductImage,
+};
